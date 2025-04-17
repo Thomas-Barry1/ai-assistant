41fb039169d93e29b6efddc8aa111bf0995cee0c
maddox05 <maddoxschmidlkofer@gmail.com>
2025-04-17 07:38:16 -0400

    added msg slicing and testing it

--- backend/functions.py
+++ backend/functions.py
@@ -14,14 +14,28 @@ def get_response(messages):
     )
     return response.output_text
 
-"""
-@ex [{"role": "user", "content": "Hello, how are you?"}]
-This is a multi-line 'comment'
-using triple quotes (docstring).
-It is often used for function documentation.
-"""
-def send_ai_messages_and_recieve_messages(messages:list):
-    resp = get_response(messages)
-    messages.append(resp)
+
+def send_ai_messages_and_recieve_messages(messages: list[str]):
+    built_messages = []
+    
+    for msg in messages:
+        built_messages.append({"role": 'user', "content": msg})
+    
+
+    # get rid of old messages 
+    built_messages = built_messages[-10:]
+    ##
+    built_messages.insert(0, {"role": 'developer', "content": "you are a flamboyant ai assistant that is very helpful and friendly. You are also a bit sassy and sarcastic. You are also a bit of a know-it-all. You are also a bit of a show-off. You are also a bit of a diva. You are also a bit of a drama queen. You are also a bit of a perfectionist. You are also a bit of a control freak. You are also a bit of a neat freak. You are also a bit of a clean freak. You are also a bit of a germaphobe. You are also a bit of a hypochondriac. You are also a bit of a worrywart. You are also a bit of a neurotic. You are also a bit of an overthinker."})
+
+    return get_response(built_messages)
+
+if __name__ == "__main__":
+    messages = ["Hello, how are you?", "my name JOHN CENA"]
+    response = send_ai_messages_and_recieve_messages(messages)
+    print(response)
+
+
+
+
 
 
